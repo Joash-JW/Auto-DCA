0beab6f9405cd3498a0847bd17ced06fe3a4d364
Jie Wei Kong <32333304+Joash-JW@users.noreply.github.com>
2022-02-02 01:39:44 +0800

    added TD implementation for get_price

--- td.py
+++ td.py
@@ -1,10 +1,12 @@
 from config import config
 from broker import Broker
-from requests import post
+from requests import post, get
 from requests.models import Response
 
 
 class TD(Broker):
+    # TD Ameritrade Implementation. Paper trading is only available via thinkorswim.
+
     def __init__(self):
         self._place_order_url = 'https://api.tdameritrade.com/v1/accounts/{}/orders'.format(config['TD_ID'])
         self._refresh_token_url = 'https://api.tdameritrade.com/v1/oauth2/token'
@@ -14,8 +16,27 @@ class TD(Broker):
             name=self.name, status_code=response.status_code, message=response.json()
         ))
 
+    def get_price(self):
+        # Refer to TD API - https://developer.tdameritrade.com/quotes/apis/get/marketdata/%7Bsymbol%7D/quotes
+        query_url = 'https://api.tdameritrade.com/v1/marketdata/{}/quotes'
+        params = {'apikey': config['TD_CONSUMER_KEY'] + '@AMER.OAUTHAP'}
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + config['TD_ACCESS_TOKEN']
+        }
+        response = get(query_url.format(config['TICKER']), params=params, headers=headers)
+        if response.status_code == 401:
+            self.refresh_token()
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + config['TD_ACCESS_TOKEN']
+        }
+        response = get(query_url.format(config['TICKER']), params=params, headers=headers)
+        return float(response.json()[config['TICKER']]['askPrice'])
+
     def refresh_token(self):
-        # refer to TD API - https://developer.tdameritrade.com/authentication/apis/post/token-0
+        # Refer to TD API - https://developer.tdameritrade.com/authentication/apis/post/token-0
         data = {
             'grant_type': 'refresh_token',
             'refresh_token': config['TD_REFRESH_TOKEN'],
@@ -34,6 +55,7 @@ class TD(Broker):
             config['TD_ACCESS_TOKEN'] = response.json()['access_token']
 
     def place_order(self):
+        # Refer to TD API - https://developer.tdameritrade.com/account-access/apis/post/accounts/%7BaccountId%7D/orders-0
         headers = {
             'Content-Type': 'application/json',
             'Authorization': 'Bearer ' + config['TD_ACCESS_TOKEN']
